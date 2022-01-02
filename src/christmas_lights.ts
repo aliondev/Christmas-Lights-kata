b108edbc7771c058bf6ac468428257a44b3b10ec
Antonio Leon <antonioleon@audiense.com>
2022-01-02 22:42:30 +0100

    Remove not required conditions

--- src/christmas_lights.ts
+++ src/christmas_lights.ts
@@ -10,28 +10,6 @@ export class ChristmasLights {
     const verticalLength = end.y - start.y + 1;
 
     this.amountLit = horizontalLength * verticalLength;
-
-    if (
-      (start.x === 0 && start.y === 0 && end.x === 999 && end.y === 0) ||
-      (start.x === 0 && start.y === 1 && end.x === 999 && end.y === 1) ||
-      (start.x === 0 && start.y === 0 && end.x === 999 && end.y === 1)
-    ) {
-
-
-      this.amountLit = horizontalLength * verticalLength;
-      return;
-    }
-
-    if (
-      (start.y === 0 && end.x === 0 && end.y === 999) ||
-      (start.y === 0 && end.x === 0 && end.y === 499) ||
-      (start.y === 500 && end.x === 0 && end.y === 999)
-    ) {
-      this.amountLit = horizontalLength * verticalLength;
-      return;
-    }
-
-    this.amountLit = horizontalLength * verticalLength;
   }
 
   turnOff(start: Coordinate, end: Coordinate) {
