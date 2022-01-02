4d8f98e2de899ffb71e866165f6e3b2e2ee38e68
Antonio Leon <antonioleon@audiense.com>
2022-01-02 21:51:53 +0100

    Hardcode implementation to turn on all the lights

--- src/christmas_lights.ts
+++ src/christmas_lights.ts
@@ -1,5 +1,16 @@
 export class ChristmasLights {
+  private amountLit: number = 0;
+
   getAmountLit() {
-    return 0;
+    return this.amountLit;
   }
+
+  turnOn(start: Coordinate, end: Coordinate) {
+    this.amountLit = 1e6;
+  }
+}
+
+type Coordinate = {
+  x: number;
+  y: number;
 }
--- test/christmas_lights.spec.ts
+++ test/christmas_lights.spec.ts
@@ -6,4 +6,14 @@ describe('Christmas lights kata', () => {
 
     expect(christmasLights.getAmountLit()).toBe(0);
   });
+
+  it('has all the lights lit if you turn on all', () => {
+    const christmasLights = new ChristmasLights();
+    const topLeftCorner = { x: 0, y: 0 };
+    const bottomRightCorner = { x: 999, y: 999};
+
+    christmasLights.turnOn(topLeftCorner, bottomRightCorner);
+
+    expect(christmasLights.getAmountLit()).toBe(1e6);
+  });
 });
